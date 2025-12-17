5a85d60a8eb7d61ea2245308332df5aa9612510f
Karl Gordon <kgordon@stsci.edu>
2025-12-17 16:05:15 -0500

    fixing rebin

--- MRSStaticRRSRF/utils/rebin_models.py
+++ MRSStaticRRSRF/utils/rebin_models.py
@@ -9,28 +9,30 @@ from MRSStaticRRSRF.utils.helpers import rebin_constres
 
 
 if __name__ == "__main__":  # pragma: no cover
-    mfiles = ["16cygb_mod_005.fits",
-              "delumi_mod_005.fits",
-              "mucol_mod_006.fits",
-              "hd2811_mod_006.fits",
-              "hd163466_mod_005.fits",
-              "hd128998_mod_004.fits",
-              ]
+    mfiles = [
+        "16cygb_mod_005.fits",
+        "delumi_mod_005.fits",
+        "mucol_mod_006.fits",
+        "hd2811_mod_006.fits",
+        "hd163466_mod_005.fits",
+        "hd128998_mod_004.fits",
+    ]
 
     for mfile in mfiles:
         print(mfile)
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=u.UnitsWarning)
             mtab = QTable.read(f"models/{mfile}")
-        mwave = mtab["WAVELENGTH"].value * u.angstrom
-        mflux = mtab["FLUX"].value * u.erg / (u.cm * u.cm * u.s * u.angstrom)
+        mwave = (mtab["WAVELENGTH"].data) * u.angstrom
+        mflux = (mtab["FLUX"].data) * u.erg / (u.cm * u.cm * u.s * u.angstrom)
         mflux = mflux.to(u.Jy, equivalencies=u.spectral_density(mwave))
 
         # rebin to R=10000 for speed
         rbres = 10000.0
         munc = np.full(len(mflux.value), 1.0)
-        wave_rebin, flux_rebin, npts_rebin = rebin_constres(
-            mwave.value, mflux.value, munc, [45000.0, 300000.0], rbres
+        waverange = np.array([4.5, 30.0]) * u.micron
+        wave_rebin, flux_rebin, uncs_rebin, npts_rebin = rebin_constres(
+            mwave, mflux.value, munc, waverange, rbres
         )
         mwave = wave_rebin * u.angstrom
         mflux = flux_rebin * u.Jy
@@ -38,4 +40,4 @@ if __name__ == "__main__":  # pragma: no cover
         otab = QTable()
         otab["wavelength"] = mwave
         otab["flux"] = mflux
-        otab.write(f"models/{mfile.replace(".fits", "_r10000.fits")}")
+        otab.write(f"models/{mfile.replace(".fits", "_r10000.fits")}", overwrite=True)
